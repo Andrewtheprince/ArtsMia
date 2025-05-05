98086a17ad3694fe0ee4d55036791dc03114e774
Andrewtheprince <andry.mazzo@outlook.com>
2025-05-05 09:21:54 +0200

    first commit

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,18 @@
 from database.DB_connect import DBConnect
+from model.artObject import ArtObject
 
 
-class DAO():
-    def __init__(self):
-        pass
+class DAO:
+
+    @staticmethod
+    def getAllNodes():
+        conn= DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        result = []
+        query= """SELECT * FROM object o"""
+        cursor.execute(query)
+        for row in cursor:
+            result.append(ArtObject(**row))
+        cursor.close()
+        conn.close()
+        return result
