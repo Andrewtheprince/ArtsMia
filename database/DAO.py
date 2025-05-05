a7e507c746c4407c59b898af9af01cf630895a5f
Andrewtheprince <andry.mazzo@outlook.com>
2025-05-05 09:58:28 +0200

    finito punto 1

--- database/DAO.py
+++ database/DAO.py
@@ -1,4 +1,5 @@
 from database.DB_connect import DBConnect
+from model.arco import Arco
 from model.artObject import ArtObject
 
 
@@ -9,10 +10,28 @@ class DAO:
         conn= DBConnect.get_connection()
         cursor = conn.cursor(dictionary=True)
         result = []
-        query= """SELECT * FROM object o"""
+        query= """SELECT * FROM objects o"""
         cursor.execute(query)
         for row in cursor:
             result.append(ArtObject(**row))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getAllArchi(idMap):
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary=True)
+        result = []
+        query = """SELECT eo.object_id as o1, eo2.object_id as o2, count(*) as peso
+                   FROM exhibition_object eo, exhibition_object eo2
+                   WHERE eo.exhibition_id = eo2.exhibition_id
+                   and eo.object_id < eo2.object_id
+                   GROUP BY eo.object_id, eo2.object_id
+                   order by peso desc"""
+        cursor.execute(query)
+        for row in cursor:
+            result.append(Arco(idMap[row["o1"]], idMap[row["o2"]], row["peso"]))
         cursor.close()
         conn.close()
         return result
--- model/model.py
+++ model/model.py
@@ -1,3 +1,29 @@
+import networkx as nx
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._graph = nx.Graph()
+        self._nodes = DAO.getAllNodes()
+        self._idMap = {}
+        for v in self._nodes:
+            self._idMap[v.object_id] = v
+
+    def buildGraph(self):
+        self._graph.add_nodes_from(self._nodes)
+        self.addAllEdges()
+
+    def addAllEdges(self):
+        allEdges = DAO.getAllArchi(self._idMap)
+        for e in allEdges:
+            self._graph.add_edge(e.o1, e.o2, weight=e.peso)
+
+    def getIdMap(self):
+        return self._idMap
+
+    def getNumNodes(self):
+        return len(self._graph.nodes)
+
+    def getNumEdges(self):
+        return len(self._graph.edges)
